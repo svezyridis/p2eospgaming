40929748f58890984c8cea9b87561c840774c86e
savvas <svezyridis@gmail.com>
2018-10-22 00:52:41 +0300

    added gitignore

--- p2peosgaming.cpp
+++ p2peosgaming.cpp
@@ -7,5 +7,5 @@ void tablegame::create(const account_name& host, const gametype game_name){
     require_auth(host);
     games existing_games(_self, _self);
     auto host_games=existing_games.get_index<N(host)>();
-    auto matching_games=host_games.lower_bound()    
+    auto matching_games=host_games.lower_bound();    
 }
