a8a8c8b3af8bd9f3803ed506d62ba33ec9c5455a
savvas <svezyridis@gmail.com>
2018-10-24 00:06:05 +0300

    change state initialization

--- score4.cpp
+++ score4.cpp
@@ -8,5 +8,13 @@ class score4game : game_base{
     }
     bool is_valid_movement(string2dvector game_state, string move_params){return true;}
     void update_state (vector<vector<string> >& game_state, string move_params){eosio::print("Savvas");}
-    string2dvector init_state(){return vector<vector<string>>(rows,vector<string>(columns,"A"));}   
+    string2dvector init_state(){
+        string2dvector state=string2dvector(rows,vector<string>(columns," "));
+        for(int row=0;row<rows;row++){
+            for(int column=0; column<columns; column++){
+                state.at(row).at(column)=std::to_string(row+1)+std::to_string(column+1);
+            }
+        }
+        return state;
+        }   
 };
